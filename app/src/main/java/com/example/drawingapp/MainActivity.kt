12bf9dfb7cefaa903fb4d903ca938f8bab0b4b1c
snehakweera77 <snehakweera77@gmail.com>
2021-07-27 22:39:07 +0530

    color panel UI added

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -3,14 +3,24 @@ package com.example.drawingapp
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private var mImageButtonCurrentPaint: ImageButton? = null
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+            mImageButtonCurrentPaint = ll_paint_colors[1] as ImageButton
+            mImageButtonCurrentPaint!!.setImageDrawable(
+                ContextCompat.getDrawable(
+                    this,
+                    R.drawable.pallet_pressed
+                )
+            )
         drawing_view.setSizeForBrush(20.toFloat());
         ib_brush.setOnClickListener{
             showBrushSizeChooserDialog()
